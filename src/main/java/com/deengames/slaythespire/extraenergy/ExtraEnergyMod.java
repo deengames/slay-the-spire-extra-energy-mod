55877be09d790f67f7e1144e1f59ffed6ef5e833
nightblade9 <nightbladecodes@gmail.com>
2023-05-18 18:29:23 -0400

    v1.2.0: play nice with other relics that increase energy.

--- src/main/java/com/deengames/slaythespire/extraenergy/ExtraEnergyMod.java
+++ src/main/java/com/deengames/slaythespire/extraenergy/ExtraEnergyMod.java
@@ -2,17 +2,16 @@ package com.deengames.slaythespire.extraenergy;
 
 import basemod.BaseMod;
 import basemod.interfaces.ISubscriber;
-import basemod.interfaces.OnPlayerTurnStartSubscriber;
+import basemod.interfaces.OnStartBattleSubscriber;
 
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 
 @SpireInitializer
-public class ExtraEnergyMod implements ISubscriber, OnPlayerTurnStartSubscriber
+public class ExtraEnergyMod implements ISubscriber, OnStartBattleSubscriber
 {
-	private static final int STARTING_ENERGY = 4;
-
 	public static void initialize() {
         new ExtraEnergyMod();
     }
@@ -22,14 +21,11 @@ public class ExtraEnergyMod implements ISubscriber, OnPlayerTurnStartSubscriber
 	}
 
 	@Override
-	public void receiveOnPlayerTurnStart()
+	public void receiveOnBattleStart(AbstractRoom currentRoom)
 	{
 		AbstractPlayer player = AbstractDungeon.player;
-		if (player.energy.energy < 4)
-		{
-			player.energy.energy = STARTING_ENERGY;
-			// Start the first round with 4/4, not 3/4 energy!
-			player.gainEnergy(1);
-		}
+		player.energy.energy++;
+		// Start the first round with 4/4, not 3/4 energy!
+		player.gainEnergy(1);
 	}
 }
